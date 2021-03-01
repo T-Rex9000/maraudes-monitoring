c7a646d06fa2e4638e60b3ab83903a92e5a40651
Antoine <antoine.deleuze@supelec.fr>
2021-03-01 22:21:36 +0100

    Major changes

--- api.py
+++ api.py
@@ -1,42 +1,106 @@
-from flask import Flask, request
-from flask_restful import Resource, Api, reqparse, abort
+from flask import Flask
+from flask_restful import Resource, Api, fields, marshal_with, abort
+from flask_sqlalchemy import SQLAlchemy
 
-from gpx_utils import parse_gpx, get_points_from_gpx
-from os.path import join, isfile
+# from gpx_utils import parse_gpx, get_points_from_gpx
+from os.path import join, dirname, abspath
+from create_db import MaraudeModel, VolunteerModel
+from arg_parsers import volunteer_put_arg_parser, maraude_put_arg_parser
 
+# Create API and load DB
 app = Flask(__name__)
 api = Api(app)
 
-maraude_path_get_arg_parser = reqparse.RequestParser()
-maraude_path_get_arg_parser.add_argument(
-    name="data_path",
-    type=str,
-    help="Path to the example data",
-    required=True
-)
+BASE_DIR = dirname(abspath(__file__))
+db_path = join(BASE_DIR, "database.db")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+db = SQLAlchemy(app)
+
+# Define how to serialize resources' outputs
+volunteer_resource_fields = {
+    "id": fields.Integer,
+    "surname": fields.String,
+    "forename": fields.String
+}
+
+maraude_resource_fields = {
+    "id": fields.Integer,
+    "volunteer_0": fields.Integer,
+    "volunteer_1": fields.Integer,
+    "volunteer_2": fields.Integer,
+    "volunteer_3": fields.Integer,
+}
 
 
-def abort_check(data_path, maraude_id):
-    if not isfile(join(data_path, f"{maraude_id}.gpx")):
-        abort("This maraude_id does not exist")
+# Define Resources
+class VolunteerResource(Resource):
+    '''
+    The marshal_with decorator is here to describe how to serialize the volunteer object
+    '''
 
+    @marshal_with(volunteer_resource_fields)
+    def get(self, volunteer_id):
+        volunteer = VolunteerModel.query.filter_by(id=volunteer_id).first()
+        if not volunteer:
+            abort(404, message="Can not find the requested volunteer id")
+        return volunteer, 200
 
-class MaraudePath(Resource):
-    def get(self, maraude_id):
-        args = maraude_path_get_arg_parser.parse_args()
-        abort_check(data_path=args["data_path"], maraude_id=maraude_id)
-        points = get_points_from_gpx(parse_gpx(join(args["data_path"], f"{maraude_id}.gpx")))
+    @marshal_with(volunteer_resource_fields)
+    def put(self, volunteer_id):
+        volunteer = VolunteerModel.query.filter_by(id=volunteer_id).first()
+        if volunteer:
+            abort(409, message="Volunteer id already exists")
 
-        return points, 200
+        args = volunteer_put_arg_parser.parse_args()
 
-    def put(self):
-        pass
+        volunteer = VolunteerModel(
+            id=volunteer_id,
+            forename=args["forename"],
+            surname=args["surname"],
+        )
+        db.session.add(volunteer)
+        db.session.commit()
+        return volunteer, 201
 
     def delete(self):
         pass
 
 
-api.add_resource(MaraudePath, '/maraude_path/<string:maraude_id>')
+class MaraudeResource(Resource):
+    @marshal_with(maraude_resource_fields)
+    def get(self, maraude_id):
+        maraude = MaraudeModel.query.filter_by(id=maraude_id).first()
+        if not maraude:
+            abort(404, message="Can not find the requested maraude id")
+        return maraude, 200
+
+    @marshal_with(maraude_resource_fields)
+    def put(self, maraude_id):
+        maraude = MaraudeModel.query.filter_by(id=maraude_id).first()
+        if maraude:
+            abort(409, message="Maraude id already exists")
+
+        args = maraude_put_arg_parser.parse_args()
+
+        maraude = MaraudeModel(
+            id=maraude_id,
+            volunteer_0=args["volunteer_0"],
+            volunteer_1=args["volunteer_1"],
+            volunteer_2=args["volunteer_2"],
+            volunteer_3=args["volunteer_3"]
+        )
+        db.session.add(maraude)
+        db.session.commit()
+        return maraude, 201
+
+    def delete(self):
+        pass
+
+
+# Add ressources to the API
+api.add_resource(VolunteerResource, '/volunteer/<string:volunteer_id>')
+api.add_resource(MaraudeResource, '/maraude/<string:maraude_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
